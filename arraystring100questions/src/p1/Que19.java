3725ad94d21ba09b3e3e301c55d3d766d4817751
Akshay Devrale <akshaydevrale@gmail.com>
2025-08-24 15:33:32 +0530

    Update Que19.java

--- arraystring100questions/src/p1/Que19.java
+++ arraystring100questions/src/p1/Que19.java
@@ -6,7 +6,6 @@ public class Que19 {
 
 	public static void main(String[] args) {
 		int num[]= {3,27,40,11,9,23,56,30};
-		int count=0;
 		
 		for(int i=0;i<num.length;i++) {
 			if(num[i]%3==0) {
@@ -16,3 +15,4 @@ public class Que19 {
 	}
 
 }
+
